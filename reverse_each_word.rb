ead9f369a3a2676695de721865d8376d3d18d55f
Marco Frisina <marco@frisina.xyz>
2019-06-29 20:08:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,4 @@
 def reverse_each_word(string)
-  split_string = string.split("")
-
-  array_length = array.length - 1
-
-  string_length.downto(0).each do |index|
-    reversed_string << string[index]
-  end
-  reversed_string
+  string.split(" ").each{|arr| arr.reverse}
+#  string.split(" ").each {|s| s.split("") }
 end
