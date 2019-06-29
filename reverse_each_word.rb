70425c8dd1a9386b47a52b3e163e1071c86275a9
Marco Frisina <marco@frisina.xyz>
2019-06-29 20:35:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,19 @@
 def reverse_each_word(string)
   string = string.split('')
   reversed_string = []
+  final_reverse = []
 
   string.each do |char|
     reversed_string.unshift(char)
   end
 
-  return reversed_string.join('')
+  first_reversal = reversed_string.join('')
+
+  first_reversal do |char|
+    final_reverse.unshift(char)
+  end
+
+  return final_reverse.join('')
+
+
 end
