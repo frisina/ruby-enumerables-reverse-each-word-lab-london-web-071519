668a5acc0f613738dd47db5915229545cfd47f1f
Marco Frisina <marco@frisina.xyz>
2019-06-29 20:41:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,19 +1,25 @@
 def reverse_each_word(string)
-  string = string.split('')
   reversed_string = []
-  final_reverse = []
 
-  string.each do |char|
+  first_array = string.split.each { |s| }
+
+  first_array.each do |char|
     reversed_string.unshift(char)
   end
+  return reversed_string
 
-  first_reversal = reversed_string.join('')
+  #string = string.split('')
+  #reversed_string = []
 
-  first_reversal.each do |char|
-    final_reverse.unshift(char)
-  end
+  #string.each do |char|
+  #  reversed_string.unshift(char)
+  #end
 
-  return final_reverse.join('')
+  #first_reversal = reversed_string.join('')
+
+
+
+  #return
 
 
 end
